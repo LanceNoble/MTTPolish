aed271f20c5fb0b8d57b9c59310a771ebdce1c78
Lance <ltnlikespie@gmail.com>
2023-09-25 22:40:10 -0400

    Reimplemented the entire path generation algorithm (again...) to incorporate backtracking

--- MTTPolish/MTTPolish/GameStuff/Board.cs
+++ MTTPolish/MTTPolish/GameStuff/Board.cs
@@ -1,208 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 namespace MTTPolish.GameStuff
 {
     internal class Board
     {
-        private Tile[,] map;
+        private char[,] map;
         private Random rndGen = new();
 
         private const int dimensions = 10;
 
         private const char grass = '.';
-        private const char left = '\u2190';
+        private const char left  = '\u2190';
         private const char right = '\u2192';
-        private const char up = '\u2191';
-        private const char down = '\u2193';
-        private const char omni = '+'; // omni tiles will determine their direction based on the previous tile in the path
+        private const char up    = '\u2191';
+        private const char down  = '\u2193';
+        //private const char omni = '+'; // omni tiles will determine their direction based on the previous tile in the path
 
+        LinkedList<(int x, int y, char dir, List<char> possibles)> path;
+
+        // To-Do: optimize this path generation, it gets mad slow once the dimensions are 30 x 30
+        // possible solution: implement a fish net graph
         public Board()
         {
-            map = new Tile[dimensions, dimensions];
+            map = new char[dimensions, dimensions];
+
             for (int i = 0; i < dimensions; i++)
             {
                 for (int j = 0; j < dimensions; j++)
-                {
-                    map[i, j] = new Tile(i, j);
-                    map[i, j].TileType = TileType.Grass;
-                }
+                    map[i, j] = grass;
             }
-            /*layout = new char[dimensions, dimensions];
+
+            path = new();
+
+            Walk(new((rndGen.Next(1, dimensions - 1), 0, right, new List<char>() { right })));
+
             for (int i = 0; i < dimensions; i++)
             {
                 for (int j = 0; j < dimensions; j++)
-                    layout[i, j] = '.';
+                    Debug.Write(map[i, j]);
+                Debug.Write('\n');
             }
-
-            // Decrease the range of possible starting X positions to ensure that the starting tile isn't spawned on the upper and lower borders of the map
-            (int x, int y, char dir) currentTile = (rndGen.Next(1, dimensions - 1), 0, right);
-
-            layout[currentTile.x, currentTile.y] = currentTile.dir;
-
-            currentTile.y++;
-            currentTile.dir = right;
-            layout[currentTile.x, currentTile.y] = currentTile.dir;
-
-            while (currentTile.y < dimensions - 1)
-            {
-                for (int i = 0; i < dimensions; i++)
-                {
-                    for (int j = 0; j < dimensions; j++)
-                    {
-                        Debug.Write(layout[i, j]);
-                    }
-                    Debug.Write("\n");
-                }
-                Debug.Write("\n");
-
-                List<char> possibilities = new() { left, right, up, down };
-
-                if (currentTile.dir == right)
-                    currentTile.y++;
-                else if (currentTile.dir == up)
-                    currentTile.x--;
-                else if (currentTile.dir == down)
-                    currentTile.x++;
-                else if (currentTile.dir == left)
-                    currentTile.y--;
-
-                if (currentTile.y == 0 || currentTile.x == 0 || currentTile.x == dimensions - 1)
-
-                if (currentTile.y - 1 > -1 && currentTile.y - 1 < dimensions  && currentTile.x > -1 && currentTile.x < dimensions && layout[currentTile.x, currentTile.y - 1] != grass)
-                    possibilities.RemoveAt(possibilities.IndexOf(left));
-                if (currentTile.y + 1 < dimensions && currentTile.y + 1 > -1 && currentTile.x > -1 && currentTile.x < dimensions && layout[currentTile.x, currentTile.y + 1] != grass)
-                    possibilities.RemoveAt(possibilities.IndexOf(right));
-                if (currentTile.x - 1 > -1 && currentTile.x - 1 < dimensions && currentTile.y > -1 && currentTile.y < dimensions && layout[currentTile.x - 1, currentTile.y] != grass)
-                    possibilities.RemoveAt(possibilities.IndexOf(up));
-                if (currentTile.x + 1 < dimensions && currentTile.x > -1 && currentTile.y > -1 && currentTile.y < dimensions && layout[currentTile.x + 1, currentTile.y] != grass)
-                    possibilities.RemoveAt(possibilities.IndexOf(down));
-
-                if (currentTile.x > -1 && currentTile.x < dimensions && currentTile.y > -1 && currentTile.y < dimensions)
-                {
-                    currentTile.dir = possibilities[rndGen.Next(0, possibilities.Count)];
-                    layout[currentTile.x, currentTile.y] = currentTile.dir;
-                }
-            }*/
-
-
-            Tile currentTile = new Tile(rndGen.Next(1, dimensions - 1), 0);
-            map[currentTile.XCoordinate, currentTile.YCoordinate] = currentTile;
-            while (currentTile.YCoordinate < dimensions - 1)
-            {
-                Print();
-
-                int nextTileXCoordinate = currentTile.XCoordinate;
-                int nextTileYCoordinate = currentTile.YCoordinate;
-                if (currentTile.TileType == TileType.Left)
-                    nextTileYCoordinate--;
-                else if (currentTile.TileType == TileType.Right)
-                    nextTileYCoordinate++;
-                else if (currentTile.TileType == TileType.Up)
-                    nextTileXCoordinate--;
-                else if (currentTile.TileType == TileType.Down)
-                    nextTileXCoordinate++;
-
-                if (nextTileXCoordinate < 0)
-                {
-
-                }
-
-                Tile nextTile = new Tile(nextTileXCoordinate, nextTileYCoordinate, currentTile);
-
-                if (nextTile.YCoordinate - 1 == -1)
-                    nextTile.RemovePossibility(TileType.Left);
-                if (nextTile.YCoordinate + 1 == dimensions)
-                    nextTile.RemovePossibility(TileType.Right);
-                if (nextTile.XCoordinate - 1 == -1)
-                    nextTile.RemovePossibility(TileType.Up);
-                if (nextTile.XCoordinate + 1 == dimensions)
-                    nextTile.RemovePossibility(TileType.Down);
-
-                if (nextTile.NextPossibleTileTypes.HasFlag(TileType.Left) && map[nextTile.XCoordinate, nextTile.YCoordinate - 1].TileType != TileType.Grass)
-                    nextTile.RemovePossibility(TileType.Left);
-                if (nextTile.NextPossibleTileTypes.HasFlag(TileType.Right) && map[nextTile.XCoordinate, nextTile.YCoordinate + 1].TileType != TileType.Grass)
-                    nextTile.RemovePossibility(TileType.Right);
-                if (nextTile.NextPossibleTileTypes.HasFlag(TileType.Up) && map[nextTile.XCoordinate - 1, nextTile.YCoordinate].TileType != TileType.Grass)
-                    nextTile.RemovePossibility(TileType.Up);
-                if (nextTile.NextPossibleTileTypes.HasFlag(TileType.Down) && map[nextTile.XCoordinate + 1, nextTile.YCoordinate].TileType != TileType.Grass)
-                    nextTile.RemovePossibility(TileType.Down);
-
-                List<TileType> possibles = Enum.GetValues<TileType>().Where(tileType => { 
-                    if (nextTile.NextPossibleTileTypes.HasFlag(tileType))
-                    {
-                        return true;
-                    }
-                    return false;
-                }).ToList(); 
-
-                nextTile.TileType = possibles[rndGen.Next(0, possibles.Count)];
-                map[nextTile.XCoordinate, nextTile.YCoordinate] = nextTile;
-
-                currentTile = nextTile;
-            }
-        } 
-
-        private void Print()
-        {
-            for (int i = 0; i < dimensions; i++)
-            {
-                for (int j = 0; j < dimensions; j++)
-                {
-                    switch (map[i, j].TileType)
-                    {
-                        case (TileType.Left):
-                            Debug.Write(left);
-                            break;
-                        case (TileType.Right):
-                            Debug.Write(right);
-                            break;
-                        case (TileType.Up):
-                            Debug.Write(up);
-                            break;
-                        case (TileType.Down):
-                            Debug.Write(down);
-                            break;
-                        case (TileType.Omni):
-                            Debug.Write(omni);
-                            break;
-                        case (TileType.Grass):
-                            Debug.Write(grass);
-                            break;
-                    }
-                }
-                Debug.Write("\n");
-            }
-            Debug.Write("\n");
+            Debug.Write('\n');
         }
 
-        /*private bool Walk(Tile currentTile)
+        private void Walk(LinkedListNode<(int x, int y, char dir, List<char> possibles)> lastTile)
         {
+            if (lastTile.Value.y == dimensions - 1)
+                return;
 
-
-            if (!Walk(currentTile))
+            if (lastTile.ValueRef.possibles.Count == 0)
             {
-
+                map[lastTile.ValueRef.x, lastTile.ValueRef.y] = grass;
+                lastTile = lastTile.Previous;
+                lastTile.ValueRef.possibles.Remove(lastTile.ValueRef.dir);
+                path.RemoveLast();
+                Walk(lastTile);
+                return;
             }
             else
             {
-                return true;
+                lastTile.ValueRef.dir = lastTile.ValueRef.possibles[rndGen.Next(0, lastTile.ValueRef.possibles.Count)];
+                map[lastTile.ValueRef.x, lastTile.ValueRef.y] = lastTile.ValueRef.dir;
             }
-            if (currentTile.Position.Y == dimensions - 1)
-                return currentTile;
 
-            if (currentTile.dir == right)
-                currentTile.y++;
-            else if (currentTile.dir == up)
-                currentTile.x--;
-            else if (currentTile.dir == down)
-                currentTile.x++;
-            else if (currentTile.dir == left)
-                currentTile.y--;
+            if (lastTile.List == null)
+                path.AddLast(lastTile);
 
-            return Walk(currentTile);
-        }*/
+            map[lastTile.Value.x, lastTile.Value.y] = lastTile.Value.dir;
+
+            (int x, int y, char dir, List<char> possibles) next = lastTile.Value;
+
+            // potential optimization: use enum flags
+            if (lastTile.Value.dir == left)
+            {
+                next.y--;
+                next.possibles = new() { up, left, down };
+            }
+            else if (lastTile.Value.dir == right)
+            {
+                next.y++;
+                next.possibles = new() { up, right, down };
+            }
+            else if (lastTile.Value.dir == up)
+            {
+                next.x--;
+                next.possibles = new() { left, up, right };
+            }
+            else if (lastTile.Value.dir == down)
+            {
+                next.x++;
+                next.possibles = new() { left, down, right };
+            }
+
+            if (next.y == -1 || next.y == dimensions || next.x == -1 || next.x == dimensions || map[next.x, next.y] != grass)
+            {
+                lastTile.ValueRef.possibles.Remove(lastTile.ValueRef.dir);
+                Walk(lastTile);
+                return;
+            }
+
+            next.dir = next.possibles[rndGen.Next(0, next.possibles.Count)];
+
+            Walk(new(next));
+        }
     }
 }
